53c89c5f01e5734263e2f05889b2ac31c0d59678
tei-k <tei@leosys.space>
2020-12-23 17:10:23 +0900

    Delete modules path

--- test/modules/awsdeploy/ca/main.tf
+++
@@ -1,9 +0,0 @@
-module "ca" {
-  source = "git::https://github.com/scalar-labs/scalar-terraform.git//modules/aws/ca?ref=master"
-
-  # Required Variables (Use remote state)
-  network = local.network
-
-  # Optional Variables
-  ca = var.ca
-}
