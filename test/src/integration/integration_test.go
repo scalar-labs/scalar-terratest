fb3c93b7f3dcb6e92625600b53bf1b64bcf243b0
tei-k <tei@leosys.space>
2021-01-08 16:31:43 +0900

    Fix var name

--- test/src/integration/integration_test.go
+++ test/src/integration/integration_test.go
@@ -149,13 +149,13 @@ func runAnsiblePlaybooks(t *testing.T) {
 	gitClone(t, "scalar-labs/scalar-kubernetes.git", k8sModuleDir)
 
   // Replace k8s custom values file
-	ReplaceCommand := shell.Command{
+	replaceCommand := shell.Command{
 		Command:    "sed",
 		Args:       []string{"-ie", "s/load-balancer-internal: \"true\"/load-balancer-internal: \"false\"/g", "./conf/scalardl-custom-values.yaml"},
 		WorkingDir: k8sModuleDir,
 	}
 
-	shell.RunCommand(t, ReplaceCommand)
+	shell.RunCommand(t, replaceCommand)
 
 	err = ioutil.WriteFile("./kube_config", []byte(lookupTargetValue(t, "kubernetes", "kube_config")), 0644)
 	if err != nil {
