89faad169dfeec58620428c90619ebe0a16a1374
tei-k <tei@leosys.space>
2021-03-11 15:55:37 +0900

    Add workflow url to description

--- .github/actions/create-jira-issue/entrypoint.sh
+++ .github/actions/create-jira-issue/entrypoint.sh
@@ -1,25 +1,21 @@
 #!/bin/sh -l
 
-date=$(date +"%Y-%m-%d")
-jira_api_url="${JIRA_BASE_URL}/rest/api/2/issue"
-jira_auth="${JIRA_USER_EMAIL}:${JIRA_API_TOKEN}"
-
 curl --request POST \
-  --url ${jira_api_url} \
-  --user ${jira_auth} \
+  --url "${JIRA_BASE_URL}/rest/api/2/issue" \
+  --user "${JIRA_USER_EMAIL}:${JIRA_API_TOKEN}" \
   --header 'Accept: application/json' \
   --header 'Content-Type: application/json' \
   --data "$(cat <<EOF
 {
   "fields": {
-    "summary": "[Terratest ${date}] Failed in ${CLOUD_ENV}",
+    "summary": "[Terratest $(date +"%Y-%m-%d")] Failed in ${CLOUD_ENV}",
     "issuetype": {
       "name": "Bug"
     },
     "project": {
       "key": "DLT"
     },
-    "description": "${JIRA_ISSUE_DESCRIPTION}",
+    "description": "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}: ${JIRA_ISSUE_DESCRIPTION}",
     "assignee": {
       "id": "${JIRA_ASSIGNEE_ID}"
     }
