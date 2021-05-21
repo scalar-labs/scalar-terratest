cfcb35f83ae50a71cd74c5918fe551e6f7e51b37
mathewseby <mathew.sebastian@scalar-labs.com>
2021-05-21 20:22:14 +0530

    Put failure tickets in active sprint

--- .github/actions/create-jira-issue/entrypoint.sh
+++ .github/actions/create-jira-issue/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sprint_id=$(curl -D- -u ${JIRA_AUTH} \
+-X GET -H "Content-Type: application/json" https://scalar-labs.atlassian.net/rest/greenhopper/1.0/sprintquery/1  | sed -n '/^{/,$p' | jq '.sprints[] | select(.state=="ACTIVE")' | jq '.id')
+
 curl --request POST \
   --url https://scalar-labs.atlassian.net/rest/api/2/issue \
   --user ${JIRA_AUTH} \
@@ -18,7 +21,8 @@ curl --request POST \
     "description": "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}: ${JIRA_ISSUE_DESCRIPTION}",
     "assignee": {
       "id": "${JIRA_ASSIGNEE_ID}"
-    }
+    },
+  "customfield_10008": ${sprint_id}
   }
 }
 EOF
