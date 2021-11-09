a599871cfadb2329b0669936fee11ddc63547d60
inv-jishnu <31100916+inv-jishnu@users.noreply.github.com>
2021-11-09 14:10:44 +0530

    Verification dashboard integration (#33)

---
+++ .github/actions/update-verification-dashboard/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+timestamp=$(date  +"%s000")
+
+curl --request POST \
+    --url http://verification-dashboard.japaneast.cloudapp.azure.com:8080/verification/details \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Bearer ${AUTOMATE_TOOL_TOKEN}" \
+    --data "$(cat <<EOF
+   {
+	"testId": "${TEST_ID}",
+	"testStatus": "${TEST_STATUS}",
+	"summary": "[Terratest $(date +"%Y-%m-%d")] Failed in ${TEST_ENVIRONMENT}",
+	"buildNumber": "",
+	"testDate": ${timestamp},
+	"commit":  "",
+	"jiraTicket": ""
+  }
+EOF
+)"
+
